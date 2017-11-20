1847f1245438c1f17e37497afeb207fc8d6bcf7b
SiLenT_Tz <shadow-jan@hotmail.de>
2017-11-20 16:49:44 +0100

    Fixed and bug

--- src/providers/http/http.ts
+++ src/providers/http/http.ts
@@ -9,9 +9,9 @@ export class HttpProvider {
 
   //Server IP
   webservice: any = "http://ec2-18-220-213-191.us-east-2.compute.amazonaws.com:8080";
-  status:any;
-  statusText:any;
-  token:any;
+  status: any;
+  statusText: any;
+  token: any;
 
   constructor(public http: Http) {
     console.log('Hello HttpProvider Provider');
@@ -22,52 +22,30 @@ export class HttpProvider {
   //send Answer
   //Post
   proof(userId, questionId, answerId) {
-      let headers = new Headers();
-      headers.append('Content-Type', 'application/json');
-      let options = new RequestOptions({ headers: headers});
-
-      return this.http.get(this.webservice+"/proof/"+userId+"/"+questionId+"/"+answerId, options);
-
-    }
-
-    //Get User Id
-    getUserId(username) {
-    let headers = new Headers();
-    /** No need to include Content-Type in Angular 4 */
-    headers.append('Content-Type', 'application/json');
-    let options = new RequestOptions({ headers: headers });
-    return this.http.get(this.webservice+"/userid/"+username, options);
+    // TODO insert HTTP request
   }
 
-    //Get Scores
-    //id:
-    getScores() {
-    let headers = new Headers();
-    /** No need to include Content-Type in Angular 4 */
-    headers.append('Content-Type', 'application/json');
-    let options = new RequestOptions({ headers: headers });
-    return this.http.get(this.webservice+"/score", options);
+  //Get User Id
+  getUserId(username) {
+    // TODO insert HTTP request
   }
 
-    //Get Question
-    //id:.....
-    getQuestion(uid) {
-    let headers = new Headers();
-    /** No need to include Content-Type in Angular 4 */
-    headers.append('Content-Type', 'application/json');
-    let options = new RequestOptions({ headers: headers });
-    return this.http.get(this.webservice+"/question/"+uid, options);
-
+  //Get Scores
+  //id:
+  getScores() {
+    // TODO insert HTTP request
   }
 
-    //Get Answer
-    //id:.... asnwers comes back
-    getAnswer(userId,questionId) {
-    let headers = new Headers();
-    /** No need to include Content-Type in Angular 4 */
-    headers.append('Content-Type', 'application/json');
-    let options = new RequestOptions({ headers: headers });
-    return this.http.get(this.webservice+"/answer/"+userId+"/"+questionId, options);
+  //Get Question
+  //id:.....
+  getQuestion(uid) {
 
+    // TODO insert HTTP request
+  }
+
+  //Get Answer
+  //id:.... asnwers comes back
+  getAnswer(userId, questionId) {
+    // TODO insert HTTP request
   }
 }
